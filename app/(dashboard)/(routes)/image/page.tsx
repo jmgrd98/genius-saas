1e142b48a98284b315715260a1ed62feab737256
João Marcelo Dantas <jmgrd98@gmail.com>
2024-03-12 17:15:41 -0300

    image generation page

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -4,7 +4,7 @@ import * as zod from 'zod';
 import Heading from "@/components/Heading"
 import { ImageIcon } from "lucide-react"
 import { useForm } from "react-hook-form"
-import { formSchema } from './constants';
+import { amountOptions, formSchema, resolutionOptions } from './constants';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
@@ -12,12 +12,10 @@ import { Button } from '@/components/ui/button';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
 import Empty from '@/components/Empty';
 import Loader from '@/components/Loader';
 import { cn } from '@/lib/utils';
-import UserAvatar from '@/components/UserAvatar';
-import BotAvatar from '@/components/BotAvatar';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 const ImagePage = () => {
     const router = useRouter();
@@ -70,19 +68,80 @@ const ImagePage = () => {
                   <FormField
                     name='prompt'
                     render={({ field }) => (
-                        <FormItem className='col-span-12 lg:col-span-10' >
+                        <FormItem className='col-span-12 lg:col-span-6' >
                             <FormControl className='m-0 p-0'>
                                 <Input 
                                     className='border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent'
                                     disabled={isLoading}
-                                    placeholder='How do I calculate the radius of a circle?'
+                                    placeholder='A picture of a horse in Swiss alps'
                                     {...field}
                                 />
                             </FormControl>
                         </FormItem>
                     )}
                   />
+                  <FormField 
+                    name='amount'
+                    control={form.control}
+                    render={({ field }) => (
+                      <FormItem className='col-span-12 lg:col-span-2'>
+                        <Select
+                          disabled={isLoading}
+                          onValueChange={field.onChange}
+                          value={field.value}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue defaultValue={field.value} />
+                            </SelectTrigger>
+                          </FormControl>
 
+                          <SelectContent>
+                          {amountOptions.map((option: any) => (
+                            <SelectItem
+                              key={option.value}
+                              value={option.value}
+                            >
+                              {option.label}
+                            </SelectItem>
+                          ))}
+                          </SelectContent>
+                        </Select>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField 
+                    name='resolution'
+                    control={form.control}
+                    render={({ field }) => (
+                      <FormItem className='col-span-12 lg:col-span-2'>
+                        <Select
+                          disabled={isLoading}
+                          onValueChange={field.onChange}
+                          value={field.value}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue defaultValue={field.value} />
+                            </SelectTrigger>
+                          </FormControl>
+
+                          <SelectContent>
+                          {resolutionOptions.map((option: any) => (
+                            <SelectItem
+                              key={option.value}
+                              value={option.value}
+                            >
+                              {option.label}
+                            </SelectItem>
+                          ))}
+                          </SelectContent>
+                        </Select>
+                      </FormItem>
+                    )}
+                  />
                   <Button
                     disabled={isLoading}
                     className='col-span-12 lg:col-span-2 w-full'>
