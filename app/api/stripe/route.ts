ce3ed82e42e5954c144d438871c2d9f79a70051c
João Marcelo Dantas <jmgrd98@gmail.com>
2024-03-13 11:54:06 -0300

    Update route.ts

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -26,7 +26,37 @@ export async function GET() {
                 customer: userSubscription.stripeCustomerId,
                 return_url: settingsUrl
             });
+
+            return new NextResponse(JSON.stringify({ url: stripeSession.url }));
         }
+
+        const stripeSession = await stripe.checkout.sessions.create({
+            success_url: settingsUrl,
+            cancel_url: settingsUrl,
+            payment_method_types: ['card'],
+            mode: 'subscription',
+            billing_address_collection: 'auto',
+            customer_email: user.emailAddresses[0].emailAddress,
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'USD',
+                        product_data: {
+                            name: 'Genius Pro',
+                            description: 'Unlimited AI Generations',
+                        },
+                        unit_amount: 2000,
+                        recurring: {
+                            interval: 'month'
+                        }
+                    },
+                    quantity: 1
+                }
+            ],
+            metadata: {
+                userId
+            }
+        })
 
     } catch (error) {
         console.error('STRIPE_ERROR', error);
