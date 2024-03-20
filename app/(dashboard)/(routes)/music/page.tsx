33b54a9e6d6f4f984074685422ad4ad5b03f438e
João Marcelo Dantas <jmgrd98@gmail.com>
2024-03-19 23:11:06 -0300

    error handling

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -16,6 +16,7 @@ import { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
 import Empty from '@/components/Empty';
 import Loader from '@/components/Loader';
 import { useProModal } from '@/hooks/use-pro-modal';
+import toast from 'react-hot-toast';
 
 const MusicPage = () => {
     const router = useRouter();
@@ -43,6 +44,8 @@ const MusicPage = () => {
         } catch (error: any) {
           if (error?.response?.status === 403) {
             proModal.onOpen();
+          } else {
+            toast.error('Something went wrong')
           }
         } finally {
           router.refresh();
