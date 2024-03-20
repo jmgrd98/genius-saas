332567ecc7bb47fd86e90847c90979736cc911ed
João Marcelo Dantas <jmgrd98@gmail.com>
2024-03-20 14:41:52 -0300

    clerk prod bug fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider isSatellite domain={(url) => url.host} signInUrl='https://genius-saas-theta.vercel.app/sign-in'>
       <html lang="en">
         <CrispProvider/>
         <body className={inter.className}>
--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,34 @@
-import { authMiddleware } from "@clerk/nextjs";
+// import { authMiddleware } from "@clerk/nextjs";
+ 
+// export default authMiddleware({
+//   // Routes that can be accessed while signed out
+//   publicRoutes: ['/', '/api/webhook'],
+// //   // Routes that can always be accessed, and have
+// //   // no authentication information
+// //   ignoredRoutes: ['/no-auth-in-this-route'],
+// });
+ 
+// export const config = {
+//   // Protects all routes, including api/trpc.
+//   // See https://clerk.com/docs/references/nextjs/auth-middleware
+//   // for more information about configuring your Middleware
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs/server";
  
 export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/api/webhook'],
-//   // Routes that can always be accessed, and have
-//   // no authentication information
-//   ignoredRoutes: ['/no-auth-in-this-route'],
+  afterAuth(auth) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: "https://genius-saas-theta.vercel.app" });
+    }
+  },
+  isSatellite: true,
+  signInUrl: 'https://talented-koala-7.clerk.accounts.dev/sign-in',
+  domain: url => url.host
 });
  
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
